c57e08fe250827c183eff2d7d32e0254c92876ba
Mohsen Hashemi <m.hashemi.code@gmail.com>
2023-03-14 07:44:10 +0330

    initialize hive db

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart' as path;
 
 import 'presentation/screens/home/home_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final dir = await path.getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+  Hive.initFlutter('hive_db');
   runApp(const MyApp());
 }
 
@@ -14,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
