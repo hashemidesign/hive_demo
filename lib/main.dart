6110ccf97179841d2a43b5aabe7e302a4a484cde
Mohsen Hashemi <m.hashemi.code@gmail.com>
2023-03-15 14:13:29 +0330

    add encryption

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_demo/models/bank.dart';
 import 'package:hive_demo/models/student.dart';
 import 'package:hive_demo/models/teacher.dart';
@@ -7,8 +10,17 @@ import 'package:path_provider/path_provider.dart' as path;
 
 import 'presentation/screens/home/home_view.dart';
 
+const secureStorage = FlutterSecureStorage();
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final encryptionKey = await secureStorage.read(key: "hiveKey");
+  if(encryptionKey == null){
+    final key = Hive.generateSecureKey();
+    await secureStorage.write(key: "hiveKey", value: base64Url.encode(key));
+  }
+  final key = base64Url.decode(encryptionKey.toString());
+
   final dir = await path.getApplicationDocumentsDirectory();
   await Hive.initFlutter(dir.path).then((_) async {
       await Hive.openBox('home');
@@ -26,7 +38,7 @@ void main() async {
 
       if (!Hive.isAdapterRegistered(2)) {
         Hive.registerAdapter<Bank>(BankAdapter());
-        await Hive.openBox<Bank>('banks');
+        await Hive.openBox<Bank>('banks', encryptionCipher: HiveAesCipher(key));
       }
   });
 
